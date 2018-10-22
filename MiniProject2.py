db3093f8b9627185e3735d313b52511d70203031
Eric <ericli21@bu.edu>
2018-10-21 21:17:56 -0400

    Draft 4

--- MiniProject2.py
+++ MiniProject2.py
@@ -16,7 +16,7 @@ val_labels = []
 test_images = []
 test_labels = []
 
-all_images = []
+all_imagefiles = []
 all_labels = []
 
 for labelname in sorted(glob.glob('./labels/*')):
@@ -28,10 +28,29 @@ for labelname in sorted(glob.glob('./labels/*')):
 	else:
 		all_labels.append(1)
 for imagename in sorted(glob.glob('./images/*')):
-	image = plt.imread(imagename)
-	image = np.asarray(image, np.float32)
-	all_images.append(image)
+	all_imagefiles.append(imagename)
 print("Done")
+
+
+
+def parsing(imagefile, label):
+	pre_image1 = tf.read_file(imagefile)
+	pre_image2 = tf.image.decode_jpeg(pre_image1, channels=3)
+	image = tf.cast(pre_image2, tf.float32)
+	return image, label
+
+dataset = tf.data.Dataset.from_tensor_slices((all_imagefiles, all_labels))
+dataset = dataset.map(parsing)
+dataset = dataset.batch(8)
+
+iterator = dataset.make_one_shot_iterator()
+images, labels = iterator.get_next()
+with tf.Session() as sess:
+	value = sess.run(images)
+	print(value)
+
+print(dataset.shape)
+'''
 
 all_images = np.array(all_images)
 all_images = all_images / 255.0
@@ -57,7 +76,7 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
 #Train model
 model.fit(train_images, train_labels, epochs=5)
 
-'''
+
 #Use validation images for accuracy
 val_loss, val_acc = model.evaluate(val_images, val_labels)
 
